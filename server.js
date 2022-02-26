ba75c89bc358d84c98aa6f44c3f7bf48637045f1
Andreas Rehné <ratiobellum@gmail.com>
2022-02-26 14:16:44 +0100

    socket.io

--- server.js
+++ server.js
@@ -3,3 +3,22 @@ var app = express();
 var server = app.listen(3000);
 app.use(express.static('public'));
 console.log('Server is running');
+
+
+var socket = require('socket.io');
+
+var io = socket(server, {
+    cors: {
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST"],
+      credentials: true,
+    },
+  });
+
+io.sockets.on('connection', newConnection);
+
+function newConnection(socket){
+    console.log('New connection: '+socket.id);
+
+
+}
